bb6009b42b5388bcce67c6c891a770db4415cba5
lucas <lucas.alek@gmail.com>
2018-03-16 15:45:24 -0500

    commit everything before i brake it

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'customers/alphabetized'
+  get 'customers/missing_email'
   get 'customers/index'
+  root to: "customers#index"
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
